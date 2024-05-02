9e79eb3dd15d73b66830d4d47c356ae23f9211ad
Predidit <34627277+Predidit@users.noreply.github.com>
2024-05-02 18:25:03 +0800

    version 1.1.8

--- lib/request/api.dart
+++ lib/request/api.dart
@@ -4,7 +4,7 @@ class Api {
   static const String domain = HttpString.baseUrl + '/';
   static const String animeList = 'https://d1zquzjgwo9yb.cloudfront.net/';
   static const String videoAPI = 'https://v.anime1.me/api';
-  static const String version = '1.1.7';
+  static const String version = '1.1.8';
   static const String sourceUrl = "https://github.com/Predidit/oneAnime";
   static const String aniDanmakuAPI = "https://ani.gamer.com.tw/ajax/danmuGet.php";
   static const String aniSearch = "https://ani.gamer.com.tw/search.php";
