d05af2209acfb249b026d6d90c2850f4bb69a60b
Predidit <34627277+Predidit@users.noreply.github.com>
2024-06-11 17:34:56 +0800

    fix boxstrains error in debug mode

--- lib/bean/anime/anime_panel.dart
+++ lib/bean/anime/anime_panel.dart
@@ -27,186 +27,165 @@ class _BangumiPanelState extends State<BangumiPanel> {
 
   @override
   Widget build(BuildContext context) {
-    return Observer(builder: (context) {
-      return Column(
-        children: [
-          Platform.isWindows || Platform.isLinux || Platform.isMacOS
-              ? Column(
-                  children: [
-                    const SizedBox(height: 7),
-                    SizedBox(
-                        height: 0,
-                        child: Text(' 正在播放：${videoController.title}'))
-                  ],
-                )
-              : Padding(
-                  padding: const EdgeInsets.only(top: 10, bottom: 6),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text('合集 '),
-                      Expanded(
-                        child: Text(
-                          ' 正在播放：${videoController.title}',
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Theme.of(context).colorScheme.outline,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      SizedBox(
-                        height: 34,
-                        child: TextButton(
-                          style: ButtonStyle(
-                            padding: MaterialStateProperty.all(EdgeInsets.zero),
-                          ),
-
-                          // Todo 展示更多
-                          onPressed: () {
-                            if (MediaQuery.sizeOf(context).height <
-                                MediaQuery.sizeOf(context).width) {
-                              SmartDialog.show(
-                                  useAnimation: false,
-                                  builder: (context) {
-                                    return AlertDialog(
-                                      title: const Text('切换选集'),
-                                      content: StatefulBuilder(builder:
-                                          (BuildContext context,
-                                              StateSetter setState) {
-                                        return Wrap(
-                                          spacing: 8,
-                                          runSpacing: 2,
-                                          children: [
-                                            for (int i = 1;
-                                                i <=
-                                                    videoController
-                                                        .token.length;
-                                                i++) ...<Widget>[
-                                              if (i ==
-                                                  videoController
-                                                      .episode) ...<Widget>[
-                                                FilledButton(
-                                                  onPressed: () async {
-                                                    SmartDialog.dismiss();
-                                                  },
-                                                  child:
-                                                      Text('第${i.toString()}话'),
-                                                ),
-                                              ] else ...[
-                                                FilledButton.tonal(
-                                                  onPressed: () async {
-                                                    videoController
-                                                        .changeEpisode(i);
-                                                    SmartDialog.dismiss();
-                                                  },
-                                                  child:
-                                                      Text('第${i.toString()}话'),
-                                                ),
-                                              ]
-                                            ]
-                                          ],
-                                        );
-                                      }),
-                                    );
-                                  });
-                            }
-                          },
+    return Expanded(
+      child: Observer(builder: (context) {
+        return Column(
+          children: [
+            Platform.isWindows || Platform.isLinux || Platform.isMacOS
+                ? const SizedBox(height: 7)
+                : Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 6),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('合集 '),
+                        Expanded(
                           child: Text(
-                            '全${videoController.token.length}话',
-                            style: const TextStyle(fontSize: 13),
+                            ' 正在播放：${videoController.title}',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-          SizedBox(
-            height: Platform.isWindows || Platform.isLinux || Platform.isMacOS
-                ? 72
-                : (MediaQuery.of(context).size.height -
-                    (MediaQuery.of(context).size.width * 9 / 16) -
-                    100),
-            // width: Platform.isWindows ? 300: null,
-            child: GridView.builder(
-              controller: listViewScrollCtr,
-              scrollDirection: Axis.vertical, // 将滚动方向改为竖直
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: Platform.isWindows || Platform.isLinux || Platform.isMacOS ? 10 : 3,
-                crossAxisSpacing: 10, // 间距
-                mainAxisSpacing: 5, // 间距
-                childAspectRatio: 1.7, // 子项宽高比
-              ),
-              itemCount: videoController.token.length,
-              itemBuilder: (BuildContext context, int i) {
-                return Container(
-                  // width: 150,
-                  margin: const EdgeInsets.only(bottom: 10), // 改为bottom间距
-                  child: Material(
-                    color: Theme.of(context).colorScheme.onInverseSurface,
-                    borderRadius: BorderRadius.circular(6),
-                    clipBehavior: Clip.hardEdge,
-                    child: InkWell(
-                      onTap: () {
-                        changeFucCall(i + 1);
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8, horizontal: 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Row(
-                              children: [
-                                if (i ==
-                                    (videoController.episode - 1)) ...<Widget>[
-                                  Image.asset(
-                                    'assets/images/live.png',
-                                    color:
-                                        Theme.of(context).colorScheme.primary,
-                                    height: 12,
-                                  ),
-                                  const SizedBox(width: 6)
-                                ],
-                                Text(
-                                  '第${i + 1}话',
-                                  style: TextStyle(
-                                      fontSize: 13,
-                                      color: i == (videoController.episode - 1)
-                                          ? Theme.of(context)
-                                              .colorScheme
-                                              .primary
-                                          : Theme.of(context)
-                                              .colorScheme
-                                              .onSurface),
-                                ),
-                                const SizedBox(width: 2),
-                              ],
+                        const SizedBox(width: 10),
+                        SizedBox(
+                          height: 34,
+                          child: TextButton(
+                            style: ButtonStyle(
+                              padding: WidgetStateProperty.all(EdgeInsets.zero),
                             ),
-                            const SizedBox(height: 3),
-                            // Todo 为标题准备
-                            // Text(
-                            //   widget.videoController.longTitle!,
-                            //   maxLines: 1,
-                            //   style: TextStyle(
-                            //       fontSize: 13,
-                            //       color: i == videoController.episode
-                            //           ? Theme.of(context).colorScheme.primary
-                            //           : Theme.of(context).colorScheme.onSurface),
-                            //   overflow: TextOverflow.ellipsis,
-                            // )
-                          ],
+      
+                            // Todo 展示更多
+                            onPressed: () {
+                              if (MediaQuery.sizeOf(context).height <
+                                  MediaQuery.sizeOf(context).width) {
+                                SmartDialog.show(
+                                    useAnimation: false,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text('切换选集'),
+                                        content: StatefulBuilder(builder:
+                                            (BuildContext context,
+                                                StateSetter setState) {
+                                          return Wrap(
+                                            spacing: 8,
+                                            runSpacing: 2,
+                                            children: [
+                                              for (int i = 1;
+                                                  i <=
+                                                      videoController
+                                                          .token.length;
+                                                  i++) ...<Widget>[
+                                                if (i ==
+                                                    videoController
+                                                        .episode) ...<Widget>[
+                                                  FilledButton(
+                                                    onPressed: () async {
+                                                      SmartDialog.dismiss();
+                                                    },
+                                                    child:
+                                                        Text('第${i.toString()}话'),
+                                                  ),
+                                                ] else ...[
+                                                  FilledButton.tonal(
+                                                    onPressed: () async {
+                                                      videoController
+                                                          .changeEpisode(i);
+                                                      SmartDialog.dismiss();
+                                                    },
+                                                    child:
+                                                        Text('第${i.toString()}话'),
+                                                  ),
+                                                ]
+                                              ]
+                                            ],
+                                          );
+                                        }),
+                                      );
+                                    });
+                              }
+                            },
+                            child: Text(
+                              '全${videoController.token.length}话',
+                              style: const TextStyle(fontSize: 13),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+            Expanded(
+              // width: Platform.isWindows ? 300: null,
+              child: GridView.builder(
+                controller: listViewScrollCtr,
+                scrollDirection: Axis.vertical, // 将滚动方向改为竖直
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: Platform.isWindows || Platform.isLinux || Platform.isMacOS ? 10 : 3,
+                  crossAxisSpacing: 10, // 间距
+                  mainAxisSpacing: 5, // 间距
+                  childAspectRatio: 1.7, // 子项宽高比
+                ),
+                itemCount: videoController.token.length,
+                itemBuilder: (BuildContext context, int i) {
+                  return Container(
+                    // width: 150,
+                    margin: const EdgeInsets.only(bottom: 10), // 改为bottom间距
+                    child: Material(
+                      color: Theme.of(context).colorScheme.onInverseSurface,
+                      borderRadius: BorderRadius.circular(6),
+                      clipBehavior: Clip.hardEdge,
+                      child: InkWell(
+                        onTap: () {
+                          changeFucCall(i + 1);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                children: [
+                                  if (i ==
+                                      (videoController.episode - 1)) ...<Widget>[
+                                    Image.asset(
+                                      'assets/images/live.png',
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      height: 12,
+                                    ),
+                                    const SizedBox(width: 6)
+                                  ],
+                                  Text(
+                                    '第${i + 1}话',
+                                    style: TextStyle(
+                                        fontSize: 13,
+                                        color: i == (videoController.episode - 1)
+                                            ? Theme.of(context)
+                                                .colorScheme
+                                                .primary
+                                            : Theme.of(context)
+                                                .colorScheme
+                                                .onSurface),
+                                  ),
+                                  const SizedBox(width: 2),
+                                ],
+                              ),
+                              const SizedBox(height: 3),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              },
-            ),
-          )
-        ],
-      );
-    });
+                  );
+                },
+              ),
+            )
+          ],
+        );
+      }),
+    );
   }
 }
